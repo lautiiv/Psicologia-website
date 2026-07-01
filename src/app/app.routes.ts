dbf2e8cb729842b00e8bb38bcaba478e0be281e2
Lautaro Villafañe <lautarovillafane98@gmail.com>
2026-07-01 14:32:25 -0300

    feat: configurar rutas y navegación desde el navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import {Home} from './pages/home/home'
+import { Appointments } from './pages/appointments/appointments';
+import { AboutMe } from './pages/about-me/about-me';
+import { Services } from './pages/services/services';
+import { Contact } from './pages/contact/contact';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"home", component:Home},
+    {path:"appointments", component:Appointments},
+    {path:'about-me', component:AboutMe},
+    {path:'services', component:Services},
+    {path:'contact', component:Contact}
+
+];
