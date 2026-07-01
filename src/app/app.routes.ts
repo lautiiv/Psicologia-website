333dfbda31fc6fc0534eb441700ce8c509e1dbe9
Lautaro Villafañe <lautarovillafane98@gmail.com>
2026-07-01 14:47:53 -0300

    Se agrega y vincula ruta por defecto y 404notfound

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,12 +4,15 @@ import { Appointments } from './pages/appointments/appointments';
 import { AboutMe } from './pages/about-me/about-me';
 import { Services } from './pages/services/services';
 import { Contact } from './pages/contact/contact';
+import {NotFound} from './pages/not-found/not-found'
 
 export const routes: Routes = [
     {path:"home", component:Home},
     {path:"appointments", component:Appointments},
     {path:'about-me', component:AboutMe},
     {path:'services', component:Services},
-    {path:'contact', component:Contact}
+    {path:'contact', component:Contact},
+    {path:"", redirectTo:"/home", pathMatch:"full"},
+    {path:"**", component:NotFound}
 
 ];
